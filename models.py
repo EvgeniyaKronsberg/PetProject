a58adac955cda83cf88ad6e7d9f7b718109335a3
Evgeniya Kronsberg <evgeniya.kronsberg@gmail.com>
2021-12-11 18:03:40 +0300

    Добавили модели Пользователь и Товар

---
+++ models.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+class Good:
+    def __init__(self, producer, model, price, stock=0):
+        self.producer = producer
+        self.model = model
+        self.producer = price
+        self.model = stock
+        

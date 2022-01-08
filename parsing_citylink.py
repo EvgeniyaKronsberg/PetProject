0ca61f5be4bad8448c61fcbc673a71fb77b5fe3c
Evgeniya Kronsberg <evgeniya.kronsberg@gmail.com>
2022-01-08 15:11:49 +0300

    Working on PET-14

--- parsing_citylink.py
+++ parsing_citylink.py
@@ -11,18 +11,49 @@ def get_html(url):
         session = HTMLSession()
         result = session.get(url)
         result.raise_for_status()
-        # session = HTMLSession()
-        # result = session.get(url)        
         return result.text
     except(requests.RequestException, ValueError):
         print('Сетевая ошибка')
         return False
 
+
+def get_search_url():
+    search_query = input('Введите название товара для отслеживания на сайте citilink.ru: ').split()
+    temp_url = 'https://www.citilink.ru/search/?text='
+    for word in search_query:
+        print(word)
+        temp_url = temp_url + word + '+'
+    url = temp_url[:-1]
+    return url
+
+
 def get_soup():
     #html = get_html(input('Введите html-ссылку для отслеживания товара на сайте citilink.ru: '))
 
-    html = get_html('https://www.citilink.ru/product/igrovaya-konsol-nintendo-switch-seryi-1183183/?text=nintendo')   #citylink
+    #html = get_html('https://www.citilink.ru/product/igrovaya-konsol-nintendo-switch-seryi-1183183/?text=nintendo')   #citylink
+
+    search_url = get_search_url()
+    html = get_html(search_url)
+    # if html:
+    #     with open("citylink_search_test.html", "w", encoding="utf8") as f:
+    #         f.write(html)
+
     return BeautifulSoup(html, 'html.parser')
+
+
+def get_search_results(soup):
+    try:
+        # found_products = soup.findAll('div')
+        
+        # found_products_grid = soup.find('div', class_='ProductCardCategoryList__grid')
+        found_products_grid = soup.find('section', class_='GroupGrid js--GroupGrid GroupGrid_has-column-gap GroupGrid_has-row-gap')
+        
+        found_products = found_products_grid.find('div', class_='product_data__gtm-js product_data__pageevents-js  ProductCardVertical js--ProductCardInListing ProductCardVertical_normal ProductCardVertical_shadow-hover ProductCardVertical_separated')
+        print(type(found_products))
+        print(found_products)
+    except AttributeError:
+        print('Ошибка поиска')
+
 
 def check_price(soup):
     try:
@@ -41,11 +72,14 @@ def check_price(soup):
 
 
 if __name__ == '__main__':
-    soup = get_soup()    
+
+    soup = get_soup()
+    
     if soup:
-        time.sleep(3)
-        price = check_price(soup)
-        search_datetime_raw = datetime.now()
-        search_datetime = search_datetime_raw.strftime('%d.%m.%Y %H:%M')
-        result = [price, search_datetime]
-        print(result)
+        time.sleep(6)
+        get_search_results(soup)
+    #     price = check_price(soup)
+    #     search_datetime_raw = datetime.now()
+    #     search_datetime = search_datetime_raw.strftime('%d.%m.%Y %H:%M')
+    #     result = [price, search_datetime]
+    #     print(result)
--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from forms import LoginForm
+from models import User
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello'
+
+# @app.route('/process-login', methods=['POST'])
+# def process_login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter(User.username == form.username.data).first()
+#         if user and user.check_password(form.password.data):
+#             login_user(user)
+#             flash('Вы успешно вошли на сайт')
+#             return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
